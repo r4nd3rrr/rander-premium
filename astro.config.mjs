b2a605d2050c809110e7bea4b1becacbe447aff2
Rander Rios <r4nd3rr10s85@gmail.com>
2025-12-23 01:06:56 -0500

    Fix Astro image service config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import { sharpImageService } from "astro/config";
 
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  image: { service: "compile"},
+  image: {
+    service: sharpImageService(),
+  },
 });
