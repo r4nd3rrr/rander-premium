27f73e571ebc244c95f09eec251589f445fa517b
Rander Rios <r4nd3rr10s85@gmail.com>
2025-12-22 21:34:13 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
+
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare()
+});
